e46aa443be66d059281e4c735625ec43b4e9d3ba
Ｍission <kaominghsuan@gmail.com>
2016-02-28 02:11:10 +0800

    add a locationManager file

---
+++ TaipeiMetroExit/TaipeiMetroLocationManager.swift
@@ -0,0 +1,13 @@
+//
+//  TaipeiMetroLocationManager.swift
+//  TaipeiMetroExit
+//
+//  Created by Ｍission on 2016/2/27.
+//  Copyright © 2016年 missionkao. All rights reserved.
+//
+
+import UIKit
+
+class TaipeiMetroLocationManager: CLLocationManager {
+
+}
